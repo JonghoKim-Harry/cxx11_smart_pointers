5ecbdea42ce1ecd2595ba6797d431a798d85668d
Jongho <scurry2119@gmail.com>
2023-05-03 11:34:20 +0900

    Person prints member variables when deleted

--- cxx11_smart_pointers/Person.h
+++ cxx11_smart_pointers/Person.h
@@ -11,7 +11,11 @@ public:
     Person() = default;
     explicit Person(const std::string& name) : name_(name) { };
 
-    ~Person() {  std::cout << "Person deallocated!" << std::endl;  }
+    ~Person() {
+        std::cout << "Person deallocated!" << std::endl;
+        printf("\tsex: %d\n\tbirth: %s\n\tname: %s\n\tnickname: %s\n", sex_, birth_.c_str(), name_.c_str(), nickname_ ==
+                nullptr ? "" : nickname_->c_str());
+    }
 
     // Members
     Sex sex_;
--- cxx11_smart_pointers/cxx_features.cpp
+++ cxx11_smart_pointers/cxx_features.cpp
@@ -2,12 +2,12 @@
 #include "cxx_pointers.h"
 
 int main() {
-    test01_allocate();
-    test02_refcount();
-    test03_reference();
-    test04_reference_for_smart_pointer();
-    test05_dangling_reference();
-    test06_unique_ptr_as_function_parameter();
+//    test01_allocate();
+//    test02_refcount();
+//    test03_reference();
+//    test04_reference_for_smart_pointer();
+//    test05_dangling_reference();
+//    test06_unique_ptr_as_function_parameter();
     test07_raw_pointer_pointing_to_unique_ptr();
 
     return 0;
