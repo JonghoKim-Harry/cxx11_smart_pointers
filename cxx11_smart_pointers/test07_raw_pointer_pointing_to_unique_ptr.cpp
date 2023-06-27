90c7c7a12a151e202073953ac4ddbc7c8dfa4a6b
Jongho <scurry2119@gmail.com>
2023-06-27 20:21:51 +0900

    Add boost-style tests

--- cxx11_smart_pointers/test07_raw_pointer_pointing_to_unique_ptr.cpp
+++ cxx11_smart_pointers/test07_raw_pointer_pointing_to_unique_ptr.cpp
@@ -8,23 +8,47 @@ static void foo(const std::string& new_name, std::unique_ptr<Person> var);
 static void bar(const std::string& new_name, std::unique_ptr<Person> *var);
 static void boo(const std::string& new_name, std::unique_ptr<Person> &var);
 
-BOOST_AUTO_TEST_CASE( test07_raw_pointers_pointing_to_unique_pointers ) {
-    BOOST_TEST_MESSAGE("START TEST07 raw pointers pointing to unique pointers");
+BOOST_AUTO_TEST_SUITE( test07_raw_pointers_pointing_to_unique_pointers )
+
+BOOST_AUTO_TEST_CASE( test07_unique_pointer_as_a_parameter_using_std_move,
+                      *boost::unit_test::expected_failures(1) ) {
+    BOOST_TEST_MESSAGE("START TEST07-01 raw pointers pointing to unique pointers");
 
     std::unique_ptr<Person> jongho(new Person(std::string("Jongho")));
     BOOST_TEST_MESSAGE("Name: " << jongho->name_);
 
-    // foo(std::string("foo"), jongho);   // COMPILATION ERROR: Call to deleted constructor of 'std::unique_ptr<Person>'
-    //foo(std::string("foo"), std::move(jongho));   // RUNTIME ERROR
+    //foo(std::string("foo"), jongho);   // COMPILATION ERROR: Call to deleted constructor of 'std::unique_ptr<Person>'
+    foo(std::string("foo"), std::move(jongho));   // RUNTIME ERROR
+    BOOST_TEST_MESSAGE("Name: " << jongho->name_);
+
+    BOOST_TEST_MESSAGE("FINISHED TEST07-01");
+}
+
+BOOST_AUTO_TEST_CASE( test07_raw_pointer_pointing_to_unique_pointer_as_a_parameter ) {
+    BOOST_TEST_MESSAGE("START TEST07-02 raw pointers pointing to unique pointer as a parameter");
+
+    std::unique_ptr<Person> jongho(new Person(std::string("Jongho")));
+    BOOST_TEST_MESSAGE("Name: " << jongho->name_);
 
     bar(std::string("bar"), &jongho);
+    BOOST_TEST_MESSAGE("Name: " << jongho->name_);
+
+    BOOST_TEST_MESSAGE("FINISHED TEST07-02");
+}
+
+BOOST_AUTO_TEST_CASE( test07_lvalue_reference_pointing_to_unique_pointer_as_a_parameter ) {
+    BOOST_TEST_MESSAGE("START TEST07-03 l-value reference_pointing to unique pointer as a parameter");
+
+    std::unique_ptr<Person> jongho(new Person(std::string("Jongho")));
     BOOST_TEST_MESSAGE("Name: " << jongho->name_);
 
     boo(std::string("boo"), jongho);
     BOOST_TEST_MESSAGE("Name: " << jongho->name_);
 
-    BOOST_TEST_MESSAGE("FINISHED TEST07 raw pointers pointing to unique pointers");
+    BOOST_TEST_MESSAGE("FINISHED TEST07-03");
 }
+
+BOOST_AUTO_TEST_SUITE_END()
 
 static void foo(const std::string& new_name, std::unique_ptr<Person> var) {
 
